25d4f78d7dccb7315d084317f5e9256e08b1d4c4
gerson <gerson.barros@m2n.com.br>
2020-07-06 15:29:46 -0300

    trabalhando com slots

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container">
+    <h1>Vue js</h1>
+    <Post>
+      <h1 slot="cabecalho">kakskas</h1>
+      <p slot="conteudo">Testando slots</p>
+      <small slot="rodape">Por Gerson Barros</small>
+    </Post>
+  </div>
+</template>
+
+<script>
+import Post from './components/Post.vue'
+export default {
+  components: {
+    Post
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .container {
+    width: 960px;
+    margin: auto;
+  }
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="post">
+        <div class="post-cabecalho">
+            <slot name="cabecalho"></slot>
+        </div>
+        <div class="post-conteudo">
+            <slot name="conteudo"></slot>
+        </div>
+        <div class="post-rodape">
+            <slot name="rodape"></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    .post {
+        margin: 15px auto;
+    }
+
+    .post-cabecalho {
+        padding: 5px 15px;
+    }
+
+    .post-conteuto {
+        font-size: 16px;
+    }
+
+    .post-rodape {
+        font-style: italic;
+    }
+</style>
