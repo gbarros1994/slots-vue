9dfcac8bfbf6647a5725d92b10e8f37fc0c54eb5
gerson <gerson.barros@m2n.com.br>
2020-07-08 00:50:57 -0300

    components dinamicos

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,58 @@
 <template>
   <div id="app" class="container">
 
-    <h1>Forma padrão</h1>
+    <h1>Components dinâmicos</h1>
     <PostsLista :posts="posts"/>
 
+    <hr>
+
     <h1>Slot com scopo</h1>
-    <PostsLista :posts="posts">
+
+    <button @click="componetSelecionado = 'Home'">Home</button>
+    <button @click="componetSelecionado = 'PostsLista'">Posts</button>
+    <button @click="componetSelecionado = 'Sobre'">Sobre</button>
+
+    <p> {{ componetSelecionado }}</p>
+
+    <component :is="componetSelecionado"
+    v-bind="propsAtuais"></component>
+
+    <Home/>
+    <Sobre/>
+    <!-- <PostsLista :posts="posts">
       <template slot-scope="{ meuPost }">
         <h2>{{ meuPost.titulo }}</h2>
         <p>{{ meuPost.conteudo }}</p>
         <small>{{ meuPost.autor }}</small>
       </template>
-    </PostsLista>
+    </PostsLista> -->
 
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue'
 import PostsLista from './components/PostsLista.vue'
+import Sobre from './components/Sobre.vue'
+
 export default {
   components: {
-    PostsLista
+    Home,
+    PostsLista,
+    Sobre
   },
   data() {
     return {
+      componetSelecionado: 'Home',
       posts: [
         { id: 1, titulo: 'Components no Vue', conteudo: 'Componetens são legais', autor: 'Gerson'},
         { id: 2, titulo: 'Components no Vue', conteudo: 'Componetens são legais', autor: 'Carlos'}
       ]
+    }
+  },
+  computed: {
+    propsAtuais() {
+      return this.componetSelecionado === 'PostsLista' ? { posts : this.posts } : {}
     }
   },
 }
